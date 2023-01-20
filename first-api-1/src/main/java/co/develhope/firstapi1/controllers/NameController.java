5325baab820059777c1b4202b07b300a8f7c76b1
Marta Aramu <marta.aramu99@gmail.com>
2023-01-20 17:54:29 +0100

    aggiunto stringbuilder

--- first-api-1/src/main/java/co/develhope/firstapi1/controllers/NameController.java
+++ first-api-1/src/main/java/co/develhope/firstapi1/controllers/NameController.java
@@ -1,6 +1,7 @@
 package co.develhope.firstapi1.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,4 +13,11 @@ public class NameController {
     public String getNameController(){
         return "My name is " + name;
     }
+
+    @PostMapping("/")
+    public String sayReverseName(){
+        StringBuilder reversedname = new StringBuilder(name);
+        return reversedname.reverse().toString();
+    }
+
 }
